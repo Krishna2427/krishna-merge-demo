c22b4cdc7ba2c8182700b6178b61a3ec431cc0f1
Krishna2427 <krishnalankapally8@gmail.com>
2025-06-15 17:34:40 +0530

    krishna1 branch: added App.js and Contact.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import Contact from './Contact';
+import ContactUs from './ContactUs';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello from krishna1 branch</h1>
+      <ContactUs />
+    </div>
+  );
+}
+
+export default App;
